b02d05413336a9c0b038358d5378dcf3bf202190
Frenz <francesco.sorrentino@grupposinergia.com>
2023-06-20 10:33:55 +0200

    init projects

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class AuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+    public function login(Request $request)
+    {
+        $this->validate($request,[
+            'email'  => 'required|max:255',
+            'password' => 'required|max:50'
+        ]);
+        $login_credits = $request->only(['email','password']);
+        $results = DB::select('Select * FROM users where password = ? AND email = ?',[$login_credits['password'],$login_credits['email']]);
+        if($results) return 'access granted!';
+        else return 'access denied';
+    }
+    //
+}
